ab2b8c1b265eba832174f38e64f5b28c782f6117
taichi <ryushi@gmail.com>
2011-12-17 23:25:10 +0900

    add sep class.

--- js/org/koshinuke/template/breadcrumb.js
+++ js/org/koshinuke/template/breadcrumb.js
@@ -13,7 +13,7 @@ org.koshinuke.template.breadcrumb.tmpl = function(opt_data, opt_sb) {
   var eListLen3 = eList3.length;
   for (var eIndex3 = 0; eIndex3 < eListLen3; eIndex3++) {
     var eData3 = eList3[eIndex3];
-    output.append('<li>', soy.$$escapeHtml(eData3), '</li>');
+    output.append('<li>', soy.$$escapeHtml(eData3), '</li>', (! (eIndex3 == eListLen3 - 1)) ? '<li class="sep"></li>' : '');
   }
   return opt_sb ? '' : output.toString();
 };
