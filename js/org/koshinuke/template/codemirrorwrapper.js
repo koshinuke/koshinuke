8ad79c6ddc6003f75ccf5663537c34b153456842
taichi <ryushi@gmail.com>
2011-12-21 04:10:11 +0900

    add edit button behavior and some hidden button.

--- js/org/koshinuke/template/codemirrorwrapper.js
+++ js/org/koshinuke/template/codemirrorwrapper.js
@@ -9,6 +9,6 @@ goog.require('soy.StringBuilder');
 
 org.koshinuke.template.codemirror.tmpl = function(opt_data, opt_sb) {
   var output = opt_sb || new soy.StringBuilder();
-  output.append('<div class="CodeMirror-toolbox"><span class="clip-container"><span class="copy"></span></span><button class="edit">edit</button><button class="raw">raw</button><button class="blame">blame</button><button class="history">history</button><span class="log username user">', soy.$$escapeHtml(opt_data.author), '</span><span class="log timestamp calendar">', soy.$$escapeHtml(opt_data.timestamp), '</span><span class="log message comment">', soy.$$escapeHtml(opt_data.message), '</span></div>');
+  output.append('<div class="CodeMirror-toolbox"><span class="clip-container"><span class="copy"></span></span><button class="edit">edit</button><button class="drop" style="display: none;">drop edit</button><button class="commit" style="display: none;">commit</button><button class="blame">blame</button><button class="history">history</button><span class="log username user">', soy.$$escapeHtml(opt_data.author), '</span><span class="log timestamp calendar">', soy.$$escapeHtml(opt_data.timestamp), '</span><span class="log message comment">', soy.$$escapeHtml(opt_data.message), '</span></div>');
   return opt_sb ? '' : output.toString();
 };
--- js/org/koshinuke/ui/CodeMirrorWrapper.js
+++ js/org/koshinuke/ui/CodeMirrorWrapper.js
@@ -4,7 +4,9 @@ goog.require('goog.array');
 goog.require('goog.dom');
 goog.require('goog.soy');
 goog.require('goog.string');
+goog.require('goog.style');
 
+goog.require('goog.ui.Button');
 goog.require('goog.ui.Component');
 
 goog.require('CodeMirror');
@@ -46,22 +48,70 @@ org.koshinuke.ui.CodeMirrorWrapper.prototype.enterDocument = function() {
 			});
 			parent.replaceChild(self.img, self.loading);
 		} else {
-			var newone = goog.soy.renderAsElement(org.koshinuke.template.codemirror.tmpl, model.node);
-			self.getElement().insertBefore(newone, self.loading);
+			var toolsEl = goog.soy.renderAsElement(org.koshinuke.template.codemirror.tmpl, model.node);
+			self.getElement().insertBefore(toolsEl, self.loading);
 			self.clip = new org.koshinuke.ui.Clipboard([resource, 'copy contents to clipboard', 'copied !!']);
-			self.clip.decorate(newone);
-			self.cm = CodeMirror(function(elt) {
-				goog.dom.classes.add(elt, "readonly");
-				parent.replaceChild(elt, self.loading);
-			}, {
+			self.clip.decorate(toolsEl);
+			self.cmOption = {
 				mode : contentType,
 				value : resource,
 				matchBrackets : true,
 				lineNumbers : true,
-				readOnly : true
-			});
+				readOnly : true,
+				onCursorActivity : function() {
+					self.cm.setLineClass(lineH, null);
+					lineH = self.cm.setLineClass(self.cm.getCursor().line, "activeline");
+				}
+			};
+			self.cm = CodeMirror(function(elt) {
+				goog.dom.classes.add(elt, "readonly");
+				parent.replaceChild(elt, self.loading);
+			}, self.cmOption);
+			var lineH = self.cm.getLineHandle(0);
+			self.setUpCMTools_(toolsEl);
 		}
 	});
+};
+/** @private */
+org.koshinuke.ui.CodeMirrorWrapper.prototype.setUpCMTools_ = function(element) {
+	goog.array.forEach(goog.dom.query('button', element), function(a) {
+		var b = new goog.ui.Button();
+		b.decorate(a);
+		b.setParent(this);
+	}, this);
+	var value = this.cm.getValue();
+	goog.events.listen(this, goog.ui.Component.EventType.ACTION, function(e) {
+		var el = e.target.getElement();
+		if(goog.dom.classes.has(el, 'edit')) {
+			value = this.cm.getValue();
+			this.toggleEdit_(element, true);
+		}
+		if(goog.dom.classes.has(el, 'drop')) {
+			this.cm.setValue(value);
+			this.toggleEdit_(element, false);
+		}
+		if(goog.dom.classes.has(el, 'commit')) {
+			// TODO commit current contents and message.
+			this.toggleEdit_(element, false);
+			// TODO reget some of resource metadatas.
+		}
+		
+	}, false, this);
+};
+org.koshinuke.ui.CodeMirrorWrapper.prototype.toggleEdit_ = function(element, editable) {
+	this.cmOption.readOnly = editable == false;
+	this.cm.setOption("readOnly", this.cmOption.readOnly);
+	if(editable) {
+		goog.dom.classes.remove(this.cm.getWrapperElement(), "readonly");
+	} else {
+		goog.dom.classes.add(this.cm.getWrapperElement(), "readonly");
+	}
+	goog.array.forEach(goog.dom.query('.drop, .commit', element), function(dc) {
+		goog.style.showElement(dc, editable);
+	}, this);
+	goog.array.forEach(goog.dom.query('.edit', element), function(dc) {
+		goog.style.showElement(dc, editable == false);
+	}, this);
 };
 /** @override */
 org.koshinuke.ui.CodeMirrorWrapper.prototype.exitDocument = function() {
