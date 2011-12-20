ffdb9d5cfe7a93a4821308e7dc0fa5cf73543d74
taichi <ryushi@gmail.com>
2011-12-20 17:14:21 +0900

    move buttons right to left.

--- js/org/koshinuke/template/codemirrorwrapper.js
+++ js/org/koshinuke/template/codemirrorwrapper.js
@@ -9,6 +9,6 @@ goog.require('soy.StringBuilder');
 
 org.koshinuke.template.codemirror.tmpl = function(opt_data, opt_sb) {
   var output = opt_sb || new soy.StringBuilder();
-  output.append('<div class="CodeMirror-toolbox"><span class="clip-container"><span class="copy"></span></span><span class="log username">', soy.$$escapeHtml(opt_data.author), '</span><span class="log timestamp">', soy.$$escapeHtml(opt_data.timestamp), '</span><span class="log message">', soy.$$escapeHtml(opt_data.message), '</span><button class="history">history</button><button class="blame">blame</button><button class="edit">edit</button></div>');
+  output.append('<div class="CodeMirror-toolbox"><span class="clip-container"><span class="copy"></span></span><button class="edit">edit</button><button class="blame">blame</button><button class="history">history</button><span class="log username">', soy.$$escapeHtml(opt_data.author), '</span><span class="log timestamp">', soy.$$escapeHtml(opt_data.timestamp), '</span><span class="log message">', soy.$$escapeHtml(opt_data.message), '</span></div>');
   return opt_sb ? '' : output.toString();
 };
