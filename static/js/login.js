025a1c758f800f5e2a7042945c4ca6f6c52a660f
taichi <ryushi@gmail.com>
2012-01-11 22:45:23 +0900

    rename form names.

--- static/js/login.js
+++ static/js/login.js
@@ -11,12 +11,11 @@ goog.exportSymbol('login', function() {
 	var form = goog.dom.query('.main .login.form')[0];
 	goog.events.listen(form, goog.events.EventType.SUBMIT, function(e) {
 		e.preventDefault();
-		var un = goog.dom.forms.getValueByName(form, 'user-name');
-		var up = goog.dom.forms.getValueByName(form, 'user-pass');
-		var rm = goog.dom.forms.getValueByName(form, 'remember');
-		console.log(un, up, rm);
+		var un = goog.dom.forms.getValueByName(form, 'u');
+		var up = goog.dom.forms.getValueByName(form, 'p');
+		var rm = goog.dom.forms.getValueByName(form, 'r');
 		if(un && up) {
-			window.location.href = 'repos.html';
+			form.submit();
 		} else {
 			goog.array.forEach(goog.dom.query('.input-pair', form), function(a) {
 				goog.dom.classes.add(a, 'error');
