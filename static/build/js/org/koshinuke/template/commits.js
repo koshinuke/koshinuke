1b8614440e1cd44316cac8446b0b5086c60633ab
taichi <ryushi@gmail.com>
2012-02-21 19:35:17 +0900

    adjust commits layout.

--- static/build/js/org/koshinuke/template/commits.js
+++ static/build/js/org/koshinuke/template/commits.js
@@ -9,6 +9,6 @@ goog.require('soy.StringBuilder');
 
 org.koshinuke.template.commits.tmpl = function(opt_data, opt_sb) {
   var output = opt_sb || new soy.StringBuilder();
-  output.append('<div class="commit"><div class="author"><img class="thumb large ', soy.$$escapeHtml(opt_data.author), '"/></div><div class="metadata"><div class="message">', soy.$$escapeHtml(opt_data.message), '</div><div class="commitdata"><span class="name">', soy.$$escapeHtml(opt_data.author), '</span>authored<span class="timestamp">', soy.$$escapeHtml(opt_data.timestamp), '</span></div></div></div>');
+  output.append('<div class="commit"><img class="author thumb large ', soy.$$escapeHtml(opt_data.author), '"/><div class="metadata"><div class="message">', soy.$$escapeHtml(opt_data.message), '</div><div class="commitdata"><span class="name">', soy.$$escapeHtml(opt_data.author), '</span>authored<span class="timestamp">', soy.$$escapeHtml(opt_data.timestamp), '</span></div></div></div>');
   return opt_sb ? '' : output.toString();
 };
