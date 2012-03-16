2c74906bd36cc0720390c97b145b081a358f266d
taichi <ryushi@gmail.com>
2012-03-16 21:35:19 +0900

    fix positioning bug.

--- static/build/js/org/koshinuke/ui/Clipboard.js
+++ static/build/js/org/koshinuke/ui/Clipboard.js
@@ -13,65 +13,68 @@ goog.require('org.koshinuke.ui.Popup');
 
 /** @constructor */
 org.koshinuke.ui.Clipboard = function(text, opt_domHelper) {
-	goog.ui.Component.call(this, opt_domHelper);
-	if(goog.isString(text)) {
-		this.text = text;
-	} else {
-		this.text = text[0];
-		this.setMessages(text[1], text[2]);
-	}
+    goog.ui.Component.call(this, opt_domHelper);
+    if (goog.isString(text)) {
+	this.text = text;
+    } else {
+	this.text = text[0];
+	this.setMessages(text[1], text[2]);
+    }
 };
 goog.inherits(org.koshinuke.ui.Clipboard, goog.ui.Component);
 
 org.koshinuke.ui.Clipboard.prototype.setCopyContents = function(text) {
-	this.text = text;
+    this.text = text;
 };
 org.koshinuke.ui.Clipboard.prototype.setMessages = function(desc, follow) {
-	this.desc = desc;
-	this.follow = follow;
+    this.desc = desc;
+    this.follow = follow;
 };
 org.koshinuke.ui.Clipboard.prototype.desc = "copy to clipboard";
 org.koshinuke.ui.Clipboard.prototype.follow = "copied !!"
 
 /** @override */
 org.koshinuke.ui.Clipboard.prototype.enterDocument = function() {
-	org.koshinuke.ui.Clipboard.superClass_.enterDocument.call(this);
-	var element = this.getElement();
-	var clip = new ZeroClipboard.Client();
-	var img = goog.dom.query('.clip-container .copy', element)[0];
-	this.popup = new org.koshinuke.ui.Popup(new org.koshinuke.positioning.GravityPosition(img, 'w', 1), 'right');
+    org.koshinuke.ui.Clipboard.superClass_.enterDocument.call(this);
+    var element = this.getElement();
+    var clip = new ZeroClipboard.Client();
+    var img = goog.dom.query('.clip-container .copy', element)[0];
+    this.popup = new org.koshinuke.ui.Popup(
+	    new org.koshinuke.positioning.GravityPosition(img, 'w', 1), 'right');
 
-	var self = this;
-	var popup = self.popup;
-	clip.addEventListener('onMouseOver', function(client) {
-		popup.setText(self.desc);
-		popup.setVisible(true);
-	});
-	clip.addEventListener('onMouseOut', function(client) {
-		popup.setVisible(false);
-	});
-	clip.addEventListener('onMouseDown', function(client) {
-		clip.setText(self.text);
-	});
-	clip.addEventListener('onComplete', function(client, text) {
-		popup.setText(self.follow);
-		popup.setVisible(true);
-	});
-	clip.glue(img, img.parentNode);
-	this.clip = clip;
-	org.koshinuke.PubSub.subscribe(org.koshinuke.PubSub.TAB_ADDED, function() {
+    var self = this;
+    var popup = self.popup;
+    clip.addEventListener('onMouseOver', function(client) {
+	popup.setText(self.desc);
+	popup.setVisible(true);
+    });
+    clip.addEventListener('onMouseOut', function(client) {
+	popup.setVisible(false);
+    });
+    clip.addEventListener('onMouseDown', function(client) {
+	clip.setText(self.text);
+    });
+    clip.addEventListener('onComplete', function(client, text) {
+	popup.setText(self.follow);
+	popup.setVisible(true);
+    });
+    clip.glue(img, img.parentNode);
+    this.clip = clip;
+    org.koshinuke.PubSub.subscribe(org.koshinuke.PubSub.TAB_ADDED, function() {
+	goog.Timer.callOnce(function() {
 	    clip.reposition(img);
-	});
+	}, 0);
+    });
 };
 /** @override */
 org.koshinuke.ui.Clipboard.prototype.exitDocument = function() {
-	org.koshinuke.ui.Clipboard.superClass_.exitDocument.call(this);
-	this.clip.destroy();
-	this.clip = null;
-	this.popup.dispose();
-	this.popup = null;
+    org.koshinuke.ui.Clipboard.superClass_.exitDocument.call(this);
+    this.clip.destroy();
+    this.clip = null;
+    this.popup.dispose();
+    this.popup = null;
 };
 /** @override */
 org.koshinuke.ui.Clipboard.prototype.disposeInternal = function() {
-	org.koshinuke.ui.Clipboard.superClass_.disposeInternal.call(this);
+    org.koshinuke.ui.Clipboard.superClass_.disposeInternal.call(this);
 };
